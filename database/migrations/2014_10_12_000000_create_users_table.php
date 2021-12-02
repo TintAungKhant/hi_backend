0fa7f2aae38d11770ba685145fad57a0cc511652
TintAungKhant <aherkersoemin@gmail.com>
2021-12-02 22:24:22 +0630

    user table update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('birthday');
+            $table->integer('gender')->default(1)->comment("1->male 2->female");
+            $table->string('avatar_url')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
