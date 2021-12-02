c68d49e6e0694ed6fc4afdea40bb002368e954fa
TintAungKhant <aherkersoemin@gmail.com>
2021-12-02 22:40:31 +0630

    added global auth_user variable (api)

--- app/Http/Controllers/Api/V1/BaseController.php
+++ app/Http/Controllers/Api/V1/BaseController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BaseController extends Controller
 {
-    //
+    protected $auth_user = null;
+
+    public function __construct() {
+        $this->middleware(function ($request, $next) {
+            $this->auth_user = Auth::user();
+            return $next($request);
+        });
+    }
 }
