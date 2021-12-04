160bec13e1b75234a5955b323d37b524b0a22e3f
TintAungKhant <aherkersoemin@gmail.com>
2021-12-04 14:20:27 +0630

    added contacts relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function contacts(){
+        return $this->belongsToMany(User::class, "contact_user", "user_id", "contact_user_id");
+    }
 }
