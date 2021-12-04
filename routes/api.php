c40c8106c37787121eb2a56c4b398ea40a2ac548
TintAungKhant <aherkersoemin@gmail.com>
2021-12-04 22:22:19 +0630

    added last seen middleware to api

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(["prefix" => "v1"], function () {
+Route::group(["prefix" => "v1", "middleware" => "update_last_seen"], function () {
     Route::post("register", [\App\Http\Controllers\Api\V1\AuthController::class, "register"]);
     Route::post("login", [\App\Http\Controllers\Api\V1\AuthController::class, "login"]);
 
