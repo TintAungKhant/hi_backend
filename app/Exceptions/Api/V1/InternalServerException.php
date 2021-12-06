e853d0887f6251dea7d7192324c9d55b4cdb2cd1
TintAungKhant <aherkersoemin@gmail.com>
2021-12-06 17:51:31 +0630

    added internal server error exception (api)

---
+++ app/Exceptions/Api/V1/InternalServerException.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Exceptions\Api\V1;
+
+use App\Traits\ApiResponseTrait;
+use Exception;
+
+class InternalServerException extends Exception
+{
+    use ApiResponseTrait;
+
+    protected $e;
+
+    public function __construct(Exception $e)
+    {
+        $this->e = $e;
+    }
+
+    public function render()
+    {
+        $data = [
+            "message" => "System error occurs"
+        ];
+
+        if(env("APP_ENV", "local") == "local"){
+            $data = [
+                "message" => $this->e->getMessage(),
+                "exception" => self::class,
+                "file" => $this->e->getFile(),
+                "line" => $this->e->getLine(),
+            ];
+        }
+
+        return $this->errorResponse($data, 500);
+    }
+}
