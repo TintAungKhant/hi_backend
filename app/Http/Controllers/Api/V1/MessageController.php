587a3e65070bfaa48f9fe68a8c59fe8e74ec9d7f
TintAungKhant <aherkersoemin@gmail.com>
2021-12-10 09:51:31 +0630

    get messages function (api)

--- app/Http/Controllers/Api/V1/MessageController.php
+++ app/Http/Controllers/Api/V1/MessageController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Exceptions\Api\V1\InternalErrorException;
+use App\Http\Requests\Api\V1\GetMessagesRequest;
+use App\Models\Message;
 use App\Traits\ApiResponseTrait;
+use Exception;
 
 class MessageController extends BaseController
 {
     use ApiResponseTrait;
+
+    public function get(GetMessagesRequest $request, $conversation_id)
+    {
+        try {
+            $last_message = null;
+            if ($request->get("last_message_id")) {
+                $last_message = Message::find($request->get("last_message_id"));
+            }
+
+            $conversation = $this->auth_user->getConversation($conversation_id);
+
+            if (!$conversation) {
+                return $this->errorResponse([
+                    "message" => "Invalid request."
+                ], 400);
+            }
+
+            $conversation->getMessages($last_message);
+
+            return $this->successResponse([
+                "conversation" => $conversation
+            ]);
+
+            return $this->successResponse([
+                "conversation" => []
+            ]);
+        } catch (Exception $e) {
+            throw new InternalErrorException($e);
+        }
+    }
 }
--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -25,4 +25,15 @@ class Conversation extends Model
     {
         return $this->hasOne(Message::class)->with("messageable")->latest();
     }
+
+    public function getMessages(?Message $last_message)
+    {
+        return $this->load(["messages" => function ($q) use ($last_message) {
+            if ($last_message) {
+                $q->where("messages.id", "<>", $last_message->id);
+                $q->where("created_at", "<=", $last_message->created_at);
+            }
+            $q->with("messageable")->limit(20)->orderBy("created_at", "DESC");
+        }])->makeHidden("pivot");
+    }
 }
